c41b87fb00866adee9c7e0fb69f7e21f79b936b0
R4bin <43667929+R4bin@users.noreply.github.com>
2021-10-25 13:28:11 +0700

    Update main.go

--- src/main.go
+++ src/main.go
@@ -28,10 +28,10 @@ import (
 //////////////////////////////////////////////////////////////////////////
 
 const (
-	IRC_Server        = "192.168.1.4:6667" //config IRC server and port here. //xxx.xxx.xxx.xxx:xxx //127.0.0.1:6667
-	IRC_Channel       = "#Test "           //config channel here. //should have space!!! //"#Example "
-	IRC_Chan_Password = ""                 //config channel password here.
-	Payload_Name      = "payload"          //config payload name.
+	IRC_Server        = "" //config IRC server and port here. //xxx.xxx.xxx.xxx:xxx //127.0.0.1:6667
+	IRC_Channel       = "" //config channel here. //should have space!!! //"#Example "
+	IRC_Chan_Password = "" //config channel password here.
+	Payload_Name      = "" //config payload name.
 )
 
 //////////////////////////////////////////////////////////////////////////
