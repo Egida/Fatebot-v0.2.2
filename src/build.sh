9b07ef519a02201e7c98ec1ce6b6e71a9a7aab46
R4bin <43667929+R4bin@users.noreply.github.com>
2021-11-06 11:25:07 -0700

    Add files via upload

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-read -p 'Please enter your payload name: ' pName
+read -p 'Enter your payload name: ' pName
 go build -ldflags "-s -w" -o $pName main.go
-echo Build success...
+echo "Build success..."
+echo "This payload is for upload on FTP server."
