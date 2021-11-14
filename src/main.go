93cb6148402c27e628f3b339b1a2af2098302290
R4bin <43667929+R4bin@users.noreply.github.com>
2021-11-14 20:47:13 +0700

    Update main.go

--- src/main.go
+++ src/main.go
@@ -42,7 +42,7 @@ func main() {
 		//Server interact
 		go func() {
 			if err != nil {
-				panic(err)
+				os.Exit(0)
 			}
 			if tools.IRC_Find(ircRead, "PING :") {
 				tools.IRC_Send(irc, "PONG "+tools.IRC_Recv(ircRead, 1))
